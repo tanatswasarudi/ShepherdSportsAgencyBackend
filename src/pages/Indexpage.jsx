7b8a006f6e7cd3370dc5eb0e9ece6a906fb27682
tanatswasarudi <tanatswasarudi09@gmail.com>
2023-07-11 12:53:48 +0530

    tee

--- src/pages/Indexpage.jsx
+++ src/pages/Indexpage.jsx
@@ -69,9 +69,9 @@ const DataProduct = useSelector((state)=>state.product.productList)
             <div className='grid md:grid-cols-4 grid-cols-1  gap-6  overflow-scroll scrollbar-none w-full'> 
            
            <div className='min-w-[280px] max-w-[280px] shadow border-t hover:bg-slate-400 border-primary bg-slate-200 rounded flex flex-col items-center px-2'><div className='w-[260px] h-[200px] '><img src={platnum} className='w-full h-full' alt=''/></div><p className='mt-2 font-sans text-base'>FC Platnum</p></div> 
-           <div className='min-w-[280px] max-w-[280px] shadow border-t hover:bg-slate-400 border-primary bg-slate-200 rounded flex flex-col items-center px-2'><div className='w-[260px] h-[200px] '><img src={dembare} className='w-full h-full' alt=''/></div><p className='mt-2 font-sans text-base'>Dynamos FC</p></div> 
+           <div className='min-w-[280px] max-w-[280px] shadow border-t hover:bg-slate-400 border-primary bg-slate-200 rounded flex flex-col items-center px-2'><div className='w-[260px] h-[220px] '><img src={dembare} className='w-full h-full' alt=''/></div><p className='mt-2 font-sans text-base'>Dynamos FC</p></div> 
            <div className='min-w-[280px] max-w-[280px] shadow border-t hover:bg-slate-400 border-primary bg-slate-200 rounded flex flex-col items-center px-2'><div className='w-[260px] h-[200px] '><img src={ngezi} className='w-full h-full' alt=''/></div><p className='mt-2 font-sans text-base'>Ngezi Platnum</p></div>  
-           <div className='min-w-[280px] max-w-[280px] shadow border-t hover:bg-slate-400 border-primary bg-slate-200 rounded flex flex-col items-center px-2'><div className='w-[260px] h-[200px] '><img src={landers} className='w-full h-full' alt=''/></div><p className='mt-2 font-sans text-base'>Highlanders FC</p></div> 
+           <div className='min-w-[280px] max-w-[280px] shadow border-t hover:bg-slate-400 border-primary bg-slate-200 rounded flex flex-col items-center px-2'><div className='w-[260px] h-[220px] '><img src={landers} className='w-full h-full' alt=''/></div><p className='mt-2 font-sans text-base'>Highlanders FC</p></div> 
             </div>
           </div>
 
@@ -79,9 +79,9 @@ const DataProduct = useSelector((state)=>state.product.productList)
             <h1 className='text-3xl font-serif font-bold py-4 text-black'>Popular Sponsors</h1>
             <div className='grid md:grid-cols-3 grid-cols-1 gap-6  overflow-scroll scrollbar-none '> 
             
-           <div className='min-w-[300px] max-w-[300px] shadow bg-slate-200 rounded flex flex-col items-center px-2'><div className='w-[260px] h-[180px] '><img src={zim} className='w-full h-full' alt=''/></div><p className='mt-2 font-sans text-base'>Castle Lager Premier Soccer League</p></div> 
+           <div className='min-w-[300px] max-w-[300px] shadow bg-slate-200 rounded flex flex-col items-center px-2'><div className='w-[260px] h-[220px] '><img src={zim} className='w-full h-full' alt=''/></div><p className='mt-2 font-sans text-base'>Castle Lager Premier Soccer League</p></div> 
            <div className='min-w-[300px] max-w-[300px] shadow bg-slate-200 rounded flex flex-col items-center px-2'><div className='w-[260px] h-[180px] '><img src={zifa} className='w-full h-full' alt=''/></div><p className='mt-2 font-sans text-base'>Zim Football Association</p></div>  
-           <div className='min-w-[300px] max-w-[300px] shadow bg-slate-200 rounded flex flex-col items-center px-2'><div className='w-[260px] h-[180px] '><img src={mbada} className='w-full h-full' alt=''/></div><p className='mt-2 font-sans text-base'>Mbada Diamond</p></div> 
+           <div className='min-w-[300px] max-w-[300px] shadow bg-slate-200 rounded flex flex-col items-center px-2'><div className='w-[260px] h-[200px] '><img src={mbada} className='w-full h-full' alt=''/></div><p className='mt-2 font-sans text-base'>Mbada Diamond</p></div> 
             </div>
             
           </div>
