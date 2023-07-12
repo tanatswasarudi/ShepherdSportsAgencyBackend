08bafabb657e37f3c2e060550cfed8a63d29c97d
tanatswasarudi <tanatswasarudi09@gmail.com>
2023-07-12 11:00:34 +0530

    sezrch

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -48,7 +48,20 @@ const ProductComponent = () => {
       {searchResults.length > 0 ? (
         <div>
           <h2 className="text-lg font-semibold mb-2">Search Results:</h2>
-          {/* Render searchResults */}
+          <div key={product.id} className=' bg-slate-200 p-2 rounded-2xl shadow shadow-black w-full min-w-[280px] max-w-[280px]  flex flex-col overflow-scroll scrollbar-none'>
+          <Link to={ `/booking/${product.id}`} onClick={()=>window.scrollTo({top:"0",behavior : "smooth"})}>
+          <div className="w-[260px] min-h-[200px] px-2 rounded-2xl">
+            <img src={product.photos[0]} className="h-full w-full" alt="" />
+          </div>
+          <h2 className='text-lg font-serif font-bold text-red-950'>{product.title}</h2>
+          <p className='flex items-center font-bold font-serif my-2 gap-2'>TEAM: {product.club}</p>
+          <p className="flex items-center font-bold font-serif my-2 gap-2">AGE: {product.age}</p>
+          <p className="flex items-center font-bold font-serif my-2 gap-2">GOALS: {product.goals}</p>
+          <p className="flex items-center font-bold font-serif my-2 gap-2">ASSISTS: {product.assists}</p>
+          <p className="flex items-center font-bold font-serif my-2 gap-2">POSITION: <span className="capitalize">{product.category}</span> </p>
+          <button className="w-[150px]  p-2 items-center rounded-full border-t border-yellow-500 shadow bg-primary">More</button>
+          </Link>
+        </div>
         </div>
       ) : (
         <div>
